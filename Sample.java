ab6bc3e8b72a3a1d7d829b4714982474f72f1a24
水谷 <mizu710tr@gmail.com>
2020-03-22 23:12:11 +0900

    コミット練習

---
+++ Sample.java
@@ -0,0 +1,11 @@
+class Sample {
+    public static void main(String[] args) {
+        Sample2 sample = new Sample2();
+    }
+}
+ 
+class Sample2 {
+    public Sample2() {
+        System.out.println("Hello Java!");
+    }
+}
