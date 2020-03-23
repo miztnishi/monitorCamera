1edb674a2af95289f4c7a224439cf4b93cb5f88a
水谷 <mizu710tr@gmail.com>
2020-03-23 23:18:10 +0900

    カメラ撮影ファイル作成(仕掛中)

---
+++ takePicture.py
@@ -0,0 +1,10 @@
+import time
+import picamera
+
+with picamera.PiCamera() as camera:
+ camera.resolution = (1024, 768)
+ camera.start_preview()
+ # Camera warm-up time
+ time.sleep(2)
+ cameras = camera.capture('sample.jpg')
+ print(cameras)
