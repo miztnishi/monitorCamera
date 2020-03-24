bfd7663a3743d97bf62e2e27c06b258f755289b4
水谷 <mizu710tr@gmail.com>
2020-03-25 00:03:04 +0900

    confファイルを追加。1日に１回ログファイルを生成。

--- notify_line_test.py
+++ notify_line_test.py
@@ -1,15 +1,28 @@
 import requests
 import os
+import logging.config
+import time
 
-token = ''
-msg = '\n動きを検知しました。\nメールを確認してください。'
-payload = {'message': msg, 'stickerPackageId': 2, 'stickerId': 22}  # 送信メッセージ
-url = 'https://notify-api.line.me/api/notify'
-headers = {'Authorization': 'Bearer ' + token}
-#files = {"imageFile":open('picture.png',"rb")}
-fileName = os.path.abspath('picture.png')
-#os.chdir(fileName)#画像保存先に移動
-files = {'imageFile': open(fileName, 'rb')}
-res = requests.post(url, params=payload, headers=headers)#,files=files)  # LINE NotifyへPOST
-print(res)
-print(files)
+
+logging.config.fileConfig('log.conf')
+# ログの出力名を設定（1）
+logger = logging.getLogger()
+
+if __name__ == '__main__':
+    while(True):
+        token = ''
+        msg = '\n動きを検知しました。\nメールを確認してください。'
+        payload = {'message': msg, 'stickerPackageId': 2, 'stickerId': 22}  # 送信メッセージ
+        url = 'https://notify-api.line.me/api/notify'
+        headers = {'Authorization': 'Bearer ' + token}
+        #files = {"imageFile":open('picture.png',"rb")}
+        fileName = os.path.abspath('picture.png')
+        #os.chdir(fileName)#画像保存先に移動
+        #files = {'imageFile': open(fileName, 'rb')}
+        res = requests.post(url, params=payload, headers=headers)#,files=files)  # LINE NotifyへPOST
+        print(res)
+        #print(files)
+
+        logger.info('info')
+        logger.warning('warning')
+        time.sleep(3)
