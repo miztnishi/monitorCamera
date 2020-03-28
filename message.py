148cb6bea390f30b7da83bb0d3de2fdcb16d1b07
水谷 <mizu710tr@gmail.com>
2020-03-29 00:14:12 +0900

    写真撮影クラス作成（テスト未実施）

--- message.py
+++ message.py
@@ -2,6 +2,7 @@
 LINE_NOTIFY_MSG = '\n動きを検知しました。\nメールを確認してください。\n[LINEから送信]'
 MAIL_SUBJECT = '動きを検知しました。{}'
 MAIL_BODY = '動きを検知しました。\n写真を確認してください'
+FILE_NAME = 'monitorCameraPicture_{}'
 
 def getMsgWithArg(arg1, arg2):
     return arg1.format(arg2)
--- takePicture.py
+++ takePicture.py
@@ -1,10 +1,24 @@
 import time
 import picamera
+import message
+import logging.config
 
-with picamera.PiCamera() as camera:
- camera.resolution = (1024, 768)
- camera.start_preview()
- # Camera warm-up time
- time.sleep(2)
- cameras = camera.capture('sample.jpg')
- print(cameras)
+class TakePicture:
+
+    logging.config.fileConfig('log.conf')
+    logger = logging.getLogger()
+    FILE_NAME = message.getMsgWithArg(message.FILE_NAME, datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
+    def takePicture:
+        logger.info('TakePictureクラス処理開始')
+        try:
+            with picamera.PiCamera() as camera:
+             camera.resolution = (1024, 768)
+             camera.start_preview()
+             # Camera warm-up time
+             time.sleep(2)
+             cameras = camera.capture(FILE_NAME)
+             logger.info('TakePictureクラス処理終了')
+             return FILE_NAME
+        except Exception e:
+            logger.error('TakePictureクラス処理失敗\n' + str(e))
+            raise e
