5e97266cbddc1827a875337c69ce516c16c92dbf
水谷 <mizu710tr@gmail.com>
2020-03-29 00:12:55 +0900

    例外処理追加

--- notify_line_test.py
+++ notify_line_test.py
@@ -6,7 +6,6 @@ import message
 import requests
 
 logging.config.fileConfig('log.conf')
-# ログの出力名を設定（1）
 logger = logging.getLogger()
 
 class SendLine:
@@ -14,16 +13,15 @@ class SendLine:
     msg = message.LINE_NOTIFY_MSG
 
     def sendLine(self):
-        payload = {'message': self.msg, 'stickerPackageId': 2, 'stickerId': 22}  # 送信メッセージ
-        url = 'https://notify-api.line.me/api/notify'
-        headers = {'Authorization': 'Bearer ' + self.token}
-        #files = {"imageFile":open('picture.png',"rb")}
-        fileName = os.path.abspath('picture.png')
-        #os.chdir(fileName)#画像保存先に移動
-        #files = {'imageFile': open(fileName, 'rb')}
-        res = requests.post(url, params=payload, headers=headers)#,files=files)  # LINE NotifyへPOST
-        print(res)
+        logger.info('SendLineクラス処理開始。')
+        try:
+            payload = {'message': self.msg, 'stickerPackageId': 2, 'stickerId': 22}  # 送信メッセージ
+            url = 'https://notify-api.line.me/api/notify'
+            headers = {'Authorization': 'Bearer ' + self.token}
 
-if __name__ == '__main__':
-    sd = SendLine()
-    sd.sendLine()
+            res = requests.post(url, params=payload, headers=headers) # LINE NotifyへPOST
+            print(res)
+            logger.info('SendLineクラス処理を終了します。')
+        except Exception as e:
+            logger.error('SendLineクラスで処理が失敗しました。\n' + str(e))
+            raise e
